dc2551e7526d3f46fe56eee871ea6868f48ff400
s24218 <s24218@pjwstk.edu.pl>
2024-03-28 21:41:17 +0100

    Gas Container implementation

---
+++ APBD2/ContainerSpace/ContainerGas.cs
@@ -0,0 +1,50 @@
+﻿using ContainerSpace;
+namespace APBD2.ContainerSpace
+{
+    internal class ContainerGas : Container, HazardNotifier
+    {
+        protected double pressure;
+        public ContainerGas(double cargoWeightKg, double heightCm, double ownWeightKg, double depthCm, double maxLoadCapacityKg, double pressure) : base(cargoWeightKg, heightCm, ownWeightKg, depthCm, maxLoadCapacityKg)
+        {
+            this.pressure = pressure;
+        }
+
+        protected override string GenerateSerialNumber()
+        {
+            lastNumber++;
+            return $"CON-G-{lastNumber}";
+        }
+
+        public override void LoadCargo(double cargoWeight)
+        {
+            if (cargoWeight > maxLoadCapacityKg)
+            {
+                throw new OverfillException("Cargo weight exceeds container's maximum load capacity.");
+            }
+           
+            NotifyDangerousSituation(serialNumber);
+
+            cargoWeightKg = cargoWeight;
+            Console.WriteLine($"Cargo loaded successfully with a weight of {cargoWeightKg} kg.");
+        }
+
+        public void NotifyDangerousSituation(string containerSerialNumber)
+        {
+            Console.WriteLine($"Potentially DANGEROUS situation detected in Gas container {containerSerialNumber}.");
+        }
+
+        public override string ToString()
+        {
+            return $"""
+                Serial Number: {serialNumber}
+                Pressure: {pressure}
+                Cargo Weight: {cargoWeightKg} kg
+                Height: {heightCm} cm
+                Own Weight: {ownWeightKg} kg
+                Depth: {depthCm} cm
+                Max Load Capacity: {maxLoadCapacityKg} kg
+
+                """;
+        }
+    }
+}
